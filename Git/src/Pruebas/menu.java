602870602176287d0df174685d03013e6af42e09
Marcos <Marcos@LAPTOP-MUBVRS9T>
2025-06-05 17:48:18 +0200

    version 1.0_A

--- Git/src/Pruebas/menu.java
+++ Git/src/Pruebas/menu.java
@@ -1,10 +1,12 @@
 package Pruebas;
 
-public class Git_1 {
+public class menu {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		System.out.println("Version1.0_A");
 	}
 
 }
